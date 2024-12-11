a6531a22d637bc0ef738ced00dd8a9a3587f77fe
Olga Dubavets <o.dubavets@gmail.com>
2024-12-11 22:21:22 +0100

    Added go to maln button

--- src/common/components/Page404/Page404.tsx
+++ src/common/components/Page404/Page404.tsx
@@ -1,10 +1,18 @@
-import s from './Page404.module.css'
+import s from "./Page404.module.css"
+import Button from "@mui/material/Button"
+import { Link } from "react-router"
+import { Path } from "common/routing/Routing"
 
 export const Page404 = () => {
     return (
         <>
             <h1 className={s.title}>404</h1>
             <h2 className={s.subTitle}>page not found</h2>
+            <div className={s.buttonBox}>
+                <Button variant={"contained"} component={Link} to={`/${Path.Login}`} sx={{ m: 0 }}>
+                    Go to main
+                </Button>
+            </div>
         </>
     )
 }
