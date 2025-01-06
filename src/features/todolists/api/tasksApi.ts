ef0dd5c034b998a12c00717f226c6c88eab33aae
Olga Dubavets <o.dubavets@gmail.com>
2025-01-06 19:34:48 +0100

    Added advanced cache tags

--- src/features/todolists/api/tasksApi.ts
+++ src/features/todolists/api/tasksApi.ts
@@ -7,7 +7,7 @@ export const tasksApi = baseApi.injectEndpoints({
     endpoints: (build) => ({
         getTasks: build.query<GetTasksResponse, { todolistId: string }>({
             query: ({ todolistId }) => `todo-lists/${todolistId}/tasks`,
-            providesTags: ["Task"],
+            providesTags: (res, err, todolistId) => [{ type: "Task", todolistId }],
         }),
         addTask: build.mutation<BaseResponse<{ item: DomainTask }>, { todolistId: string; title: string }>({
             query: ({ todolistId, title }) => {
@@ -17,7 +17,7 @@ export const tasksApi = baseApi.injectEndpoints({
                     body: { title },
                 }
             },
-            invalidatesTags: ["Task"],
+            invalidatesTags: (res, err, { todolistId }) => [{ type: "Task", id: todolistId }],
         }),
         removeTask: build.mutation<BaseResponse, { todolistId: string; taskId: string }>({
             query: ({ todolistId, taskId }) => {
@@ -26,12 +26,9 @@ export const tasksApi = baseApi.injectEndpoints({
                     url: `todo-lists/${todolistId}/tasks/${taskId}`,
                 }
             },
-            invalidatesTags: ["Task"],
+            invalidatesTags: (res, err, { todolistId }) => [{ type: "Task", id: todolistId }],
         }),
-        updateTask: build.mutation<
-            BaseResponse<{ item: DomainTask }>,
-            { task: DomainTask }
-        >({
+        updateTask: build.mutation<BaseResponse<{ item: DomainTask }>, { task: DomainTask }>({
             query: ({ task }) => {
                 const model: UpdateTaskModel = {
                     status: task.status,
@@ -47,7 +44,7 @@ export const tasksApi = baseApi.injectEndpoints({
                     body: model,
                 }
             },
-            invalidatesTags: ["Task"],
+            invalidatesTags: (res, err, { task }) => [{ type: 'Task', id: task.todoListId }],
         }),
     }),
 })
